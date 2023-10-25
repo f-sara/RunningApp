525537513ef782ba1f2cc49b5cc55875785be19a
f-sara <jyujyusa.68@gmail.com>
2023-10-25 10:23:02 +0900

    Initial Commit

---
+++ RunningApp/RunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppApp.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
