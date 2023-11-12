8146fe6e330f890f05185741bde4819428f2cae6
f-sara <jyujyusa.68@gmail.com>
2023-11-12 11:42:42 +0900

    add: WeatherShowViewを追加

---
+++ RunningApp/View/Views/WeatherShowView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherShowView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/12.
+//
+
+import SwiftUI
+
+struct WeatherShowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherShowView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherShowView()
+    }
+}
