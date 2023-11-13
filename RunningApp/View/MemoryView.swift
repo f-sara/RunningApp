ba6090b1417f9a4f53f8815b8fdfeb6108f536dd
f-sara <jyujyusa.68@gmail.com>
2023-11-13 16:02:29 +0900

    add: MemoryViewを追加

---
+++ RunningApp/View/MemoryView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/13.
+//
+
+import SwiftUI
+
+struct MemoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoryView()
+    }
+}
