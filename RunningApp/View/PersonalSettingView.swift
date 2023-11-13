92195449820d5ca02c0f9c3dc0a88463309b32b1
f-sara <jyujyusa.68@gmail.com>
2023-11-13 16:02:59 +0900

    add: PersonalSettingViewを追加

---
+++ RunningApp/View/PersonalSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalSettingView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/13.
+//
+
+import SwiftUI
+
+struct PersonalSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonalSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalSettingView()
+    }
+}
