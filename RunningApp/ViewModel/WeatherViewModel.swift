272bd84eb37ee08c73531ee35d468260ce152de1
f-sara <jyujyusa.68@gmail.com>
2023-11-18 15:33:44 +0900

    refactor: WeatherAPIClientの追加に伴い関連するViewModelを修正

--- RunningApp/ViewModel/WeatherViewModel.swift
+++ RunningApp/ViewModel/WeatherViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  WeatherController.swift
+//  WeatherViewModel.swift
 //  RunningApp
 //
 //  Created by 藤井紗良 on 2023/11/13.
@@ -7,22 +7,26 @@
 
 import Foundation
 
-class WeatherController: ObservableObject {
-    @Published var weatherData: WeatherData?
-    @Published var weatherScore: Double = 0.0
-    @Published var weatherImageURL: String = ""
+final class WeatherViewModel: ObservableObject {
 
-    private let weatherAPI = WeatherAPI()
+    @Published var weatherData: WeatherDataModel?
 
-    func fetchWeatherData(latitude: Double, longitude: Double) {
-        weatherAPI.fetchWeatherData(latitude: latitude, longitude: longitude) { result in
+    private let weatherAPIClient = WeatherAPIClient()
+
+    private let latitude = 35.6895
+    private let longitude = 139.6917
+
+    var otherWeatherData: WeatherModel? {
+        guard let weatherData = weatherData else {return nil}
+
+        return WeatherModel.init(weatherScore: 0.81 * weatherData.main.temp + 0.01 * weatherData.main.humidity * (0.99 * weatherData.main.temp - 14.3) + 46.3, weatherImageURL: "https://openweathermap.org/img/wn/\(weatherData.weather[0].icon)@2x.png")
+    }
+
+    func fetchWeatherData() {
+        weatherAPIClient.fetchWeatherData(latitude: latitude, longitude: longitude) { result in
             switch result {
             case .success(let data):
                 self.weatherData = data
-                if let weatherData = self.weatherData {
-                    self.weatherScore = 0.81 * weatherData.main.temperature + 0.01 * weatherData.main.humidity * (0.99 * weatherData.main.temperature - 14.3) + 46.3 // 3. weatherScore の計算を追加
-                    self.weatherImageURL = "https://openweathermap.org/img/wn/\(weatherData.weather[0].icon)@2x.png"
-                }
             case .failure(let error):
                 print("API Error: \(error)")
             }
