37eb8be0875afd4082d747e30f7aef76a1368aae
f-sara <jyujyusa.68@gmail.com>
2023-11-21 09:41:47 +0900

    add: ChartViewを追加

---
+++ RunningApp/View/Views/MemoryView/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
