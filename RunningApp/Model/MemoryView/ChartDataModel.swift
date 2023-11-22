f9f5e5d2733be7db490d0960da656aa93d7991a6
f-sara <jyujyusa.68@gmail.com>
2023-11-22 09:39:47 +0900

    feat: ChartDataModelをデータの種類ごとに分割

--- RunningApp/Model/MemoryView/ChartDataModel.swift
+++ RunningApp/Model/MemoryView/ChartDataModel.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-struct ChartDataModel: Identifiable {
-    var id = UUID()
-    var day: String
-    var value: Int?
-    
+struct ChartDataModel {
+    struct StepDataModel: Identifiable {
+        var id = UUID()
+        var day: String
+        var step: Int?
+    }
+
+    struct KcalDataModel: Identifiable {
+        var id = UUID()
+        var day: String
+        var kcal: Double?
+    }
+
+    struct DistanceDataModel: Identifiable {
+        var id = UUID()
+        var day: String
+        var distance: Double?
+    }
 }
