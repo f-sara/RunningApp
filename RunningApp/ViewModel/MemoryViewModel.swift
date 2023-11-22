76179475b85f5cbb2d12ab359d0e1f747881ce1b
f-sara <jyujyusa.68@gmail.com>
2023-11-22 10:10:53 +0900

    feat: Modelの変更に伴って修正

--- RunningApp/ViewModel/MemoryViewModel.swift
+++ RunningApp/ViewModel/MemoryViewModel.swift
@@ -9,16 +9,17 @@ import Foundation
 
 final class MemoryViewModel: ObservableObject {
     @Published var healthDataModel = HealthDataModel()
-    @Published var chartDataModel: [ChartDataModel] = []
+    @Published var stepDataModel: [ChartDataModel.StepDataModel] = []
+    @Published var kcalDataModel: [ChartDataModel.KcalDataModel] = []
+    @Published var distanceDataModel: [ChartDataModel.DistanceDataModel] = []
 
-
-    func updateLineData(stepCounts: [Int]) {
+    func updateStepData(stepCounts: [Int]) {
         let today = Date()
         let calendar = Calendar.current
         let startDay = calendar.date(byAdding: .day, value: -6, to: today)!
         var date = startDay
 
-        var data: [ChartDataModel] = []
+        var data: [ChartDataModel.StepDataModel] = []
 
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM/dd"
@@ -26,20 +27,107 @@ final class MemoryViewModel: ObservableObject {
 
         for (_, steps) in stepCounts.enumerated() {
             let dayString = dateFormatter.string(from: date)
-            data.append(ChartDataModel(day: dayString, value: steps))
+            data.append(ChartDataModel.StepDataModel(day: dayString, step: steps))
             date = calendar.date(byAdding: .day, value: 1, to: date)!
         }
 
-        chartDataModel = data
+        stepDataModel = data
     }
 
-    func onAppearMemoryView() {
+    func updateKcalData(kcalData: [Double]) {
+        let today = Date()
+        let calendar = Calendar.current
+        let startDay = calendar.date(byAdding: .day, value: -6, to: today)!
+        var date = startDay
+
+        var data: [ChartDataModel.KcalDataModel] = []
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd"
+
+
+        for (_, kcal) in kcalData.enumerated() {
+            let dayString = dateFormatter.string(from: date)
+            data.append(ChartDataModel.KcalDataModel(day: dayString, kcal: kcal))
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+        }
+
+        kcalDataModel = data
+    }
+
+    func updateDistanceData(distanceData: [Double]) {
+        let today = Date()
+        let calendar = Calendar.current
+        let startDay = calendar.date(byAdding: .day, value: -6, to: today)!
+        var date = startDay
+
+        var data: [ChartDataModel.DistanceDataModel] = []
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd"
+
+
+        for (_, distance) in distanceData.enumerated() {
+            let dayString = dateFormatter.string(from: date)
+            data.append(ChartDataModel.DistanceDataModel(day: dayString, distance: distance))
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+        }
+
+        distanceDataModel = data
+    }
+    
+
+    func onAppearStepMemoryView() {
         healthDataModel.requestHealthAuthorization { success in
             if success {
-                self.healthDataModel.fetchStepsData { stepCounts in
-                    self.updateLineData(stepCounts: stepCounts)
+                self.healthDataModel.fetchStepsData { result in
+                    switch result {
+                    case .success(let stepCounts):
+                        self.updateStepData(stepCounts: stepCounts)
+                    case .failure(let error):
+                        print(error)
+                    }
                 }
+            } else {
+                print("アクセスが許可されていません")
             }
         }
     }
+
+    func onAppearKcalMemoryView() {
+        healthDataModel.requestHealthAuthorization { success in
+            if success {
+                self.healthDataModel.fetchKcalData { result in
+                    switch result {
+                    case .success(let kcal):
+                        self.updateKcalData(kcalData: kcal)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            } else {
+                print("アクセスが許可されていません")
+            }
+        }
+    }
+
+    func onAppearDistanceMemoryView() {
+        healthDataModel.requestHealthAuthorization { success in
+            if success {
+                self.healthDataModel.fetchDistanceData { result in
+                    switch result {
+                    case .success(let distance):
+                        self.updateDistanceData(distanceData: distance)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            } else {
+                print("アクセスが許可されていません")
+            }
+        }
+    }
+
 }
+
+
