519c30c89d647e1cf0fc7aecc055265ea94fa480
f-sara <jyujyusa.68@gmail.com>
2023-11-22 11:50:19 +0900

    add: ChartDataViewを追加

---
+++ RunningApp/View/Views/MemoryView/ChartDataView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartDataView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/22.
+//
+
+import SwiftUI
+
+struct ChartDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartDataView()
+    }
+}
