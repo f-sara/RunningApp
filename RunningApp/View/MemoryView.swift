bc0008c3ffd4c12614fab12dbf196949cc3fad69
f-sara <jyujyusa.68@gmail.com>
2023-11-22 09:56:31 +0900

    feat: 作成したChartViewを仮配置

--- RunningApp/View/MemoryView.swift
+++ RunningApp/View/MemoryView.swift
@@ -9,34 +9,13 @@ import SwiftUI
 import Charts
 
 struct MemoryView: View {
-    @ObservedObject var memoryViewModel = MemoryViewModel()
 
     var body: some View {
-        VStack {
-
-            Chart(memoryViewModel.chartDataModel) { dataRow in
-
-                BarMark (
-                    x: .value("曜日", dataRow.day),
-                    y: .value("歩数", dataRow.value ?? 0)
-                )
-                .foregroundStyle(.cyan)
-
-                PointMark (
-                    x: .value("曜日", dataRow.day),
-                    y: .value("歩数", dataRow.value ?? 0)
-                )
-                .foregroundStyle(.green)
-
-                LineMark(
-                    x: .value("曜日", dataRow.day),
-                    y: .value("歩数", dataRow.value ?? 0)
-                )
-                .foregroundStyle(.green)
-            }
-            .frame(height: 300)
-            .onAppear {
-                memoryViewModel.onAppearMemoryView()
+        ScrollView {
+            VStack {
+                ChartView(typeOfData: .step)
+                ChartView(typeOfData: .kcal)
+                ChartView(typeOfData: .distance)
             }
         }
     }
