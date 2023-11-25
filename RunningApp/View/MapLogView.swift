cb3ca89fe0b9dc41dc26b829c29b95ae6638e4d3
f-sara <jyujyusa.68@gmail.com>
2023-11-25 17:43:49 +0900

    add: MapLogViewを追加

---
+++ RunningApp/View/MapLogView.swift
@@ -0,0 +1,20 @@
+//
+//  MapLogView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/25.
+//
+
+import SwiftUI
+
+struct MapLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapLogView()
+    }
+}
