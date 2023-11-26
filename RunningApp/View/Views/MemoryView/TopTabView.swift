d80c527e98a4fd75d938c1c60db112e2115a3076
f-sara <jyujyusa.68@gmail.com>
2023-11-26 18:32:31 +0900

    add: TopTabViewを追加

---
+++ RunningApp/View/Views/MemoryView/TopTabView.swift
@@ -0,0 +1,20 @@
+//
+//  TopTabView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/26.
+//
+
+import SwiftUI
+
+struct TopTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopTabView()
+    }
+}
