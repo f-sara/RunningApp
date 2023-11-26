ab7bd9aa7c780be7b82679cc3f7184677cafd54c
f-sara <jyujyusa.68@gmail.com>
2023-11-26 15:50:30 +0900

    add: FinishButtonViewを追加

---
+++ RunningApp/View/Views/ResultView/FinishButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  FinishButtonView.swift
+//  RunningApp
+//
+//  Created by 藤井紗良 on 2023/11/26.
+//
+
+import SwiftUI
+
+struct FinishButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinishButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinishButtonView()
+    }
+}
