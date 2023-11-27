20f8f2342caec1e10ba3f62a37a063ad7be46a30
f-sara <jyujyusa.68@gmail.com>
2023-11-27 10:54:08 +0900

    feat: TopTabViewを作成

--- RunningApp/View/Views/MemoryView/TopTabView.swift
+++ RunningApp/View/Views/MemoryView/TopTabView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct TopTabView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    let list: [String]
+    @Binding var selectedTab: Int
 
-struct TopTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        TopTabView()
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0 ..< list.count, id: \.self) { row in
+                Button(action: {
+                    withAnimation {
+                        selectedTab = row
+                    }
+                }, label: {
+                    VStack(spacing: 0) {
+                        HStack {
+                            Text(list[row])
+                                .foregroundColor(Color.black)
+                                .font(.system(size: 14))
+                        }
+                        .frame(
+                            width: (UIScreen.main.bounds.width / CGFloat(list.count)),
+                            height: 48 - 3
+                        )
+                        Rectangle()
+                            .fill(selectedTab == row ? Color.green : Color.clear)
+                            .frame(height: 3)
+                    }
+                    .fixedSize()
+                })
+            }
+        }
+        .frame(height: 38)
+        .background(Color.white)
+        .compositingGroup()
+        .shadow(color: .primary.opacity(0.03), radius: 2, x: 4, y: 4)
     }
 }
