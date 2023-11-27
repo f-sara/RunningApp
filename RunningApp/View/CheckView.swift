33b0abe274d8711bff33f1b56dda6b1267687e8d
f-sara <jyujyusa.68@gmail.com>
2023-11-27 13:59:11 +0900

    feat: 余白を調整

--- RunningApp/View/CheckView.swift
+++ RunningApp/View/CheckView.swift
@@ -21,24 +21,25 @@ struct CheckView: View {
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color("white"))
-                        .frame(width: 340, height: 210)
+                        .frame(width: 320, height: 200)
                     VStack(spacing: 0) {
                         Text("ルートの記録を始めますか？")
                             .foregroundColor(Color("darkblue"))
-                            .font(.system(size: 20))
+                            .font(.system(size: 20.5))
                             .padding(.top, 8)
-                            .padding(.bottom, 45)
+                            .padding(.bottom, 35)
                         NavigationLink {
                             MapLogView()
                         } label: {
                             NormalButtonView(buttonText: "開始")
                         }
-                        .padding(.bottom, 25)
+                        .padding(.bottom, 21)
                         Button {
                             dismiss()
                         } label: {
                             Text("キャンセル")
                         }
+                        .padding(.bottom, 8)
                     }
 
                 }
