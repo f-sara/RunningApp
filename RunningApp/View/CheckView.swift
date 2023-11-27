b57b58e7e225b0f4c227e9ecb748831df95292d5
f-sara <jyujyusa.68@gmail.com>
2023-11-27 13:55:01 +0900

    feat: ルート記録開始前の確認画面を作成

--- RunningApp/View/CheckView.swift
+++ RunningApp/View/CheckView.swift
@@ -8,24 +8,50 @@
 import SwiftUI
 
 struct CheckView: View {
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack {
-            NavigationStack {
-                NavigationLink {
-                    MapLogView()
-                } label: {
-                    Text("計測開始")
+
+        NavigationStack {
+            VStack(spacing: 0) {
+                Image("2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 240)
+                    .padding(.bottom, 50)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color("white"))
+                        .frame(width: 340, height: 210)
+                    VStack(spacing: 0) {
+                        Text("ルートの記録を始めますか？")
+                            .foregroundColor(Color("darkblue"))
+                            .font(.system(size: 20))
+                            .padding(.top, 8)
+                            .padding(.bottom, 45)
+                        NavigationLink {
+                            MapLogView()
+                        } label: {
+                            NormalButtonView(buttonText: "開始")
+                        }
+                        .padding(.bottom, 25)
+                        Button {
+                            dismiss()
+                        } label: {
+                            Text("キャンセル")
+                        }
+                    }
+
                 }
+
             }
+            .navigationBarBackButtonHidden(true)
         }
-        .interactiveDismissDisabled()
-        .navigationBarBackButtonHidden(true)
-
     }
-}
 
-struct CheckView_Previews: PreviewProvider {
-    static var previews: some View {
-        CheckView()
+    struct CheckView_Previews: PreviewProvider {
+        static var previews: some View {
+            CheckView()
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
